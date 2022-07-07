47cc77bfebf9141f81d7200d7e75b6292faaf4f9
isaoud <imedsaoud@factonics.com>
2022-07-07 13:35:56 +0200

    Added a test schematic for new user to have basic understanding of schematic execution

--- app/src/app/index.ts
+++ app/src/app/index.ts
@@ -3,8 +3,9 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function app(_options: any): Rule {
+export function test(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create('./generations/app_test/example.js', `console.log('Hello World');`);
     return tree;
   };
 }
