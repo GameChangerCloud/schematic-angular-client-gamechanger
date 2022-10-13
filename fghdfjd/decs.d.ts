05e99063b94853059c80270776df5b8e90cd5278
isaoud <imedsaoud@factonics.com>
2022-10-13 13:30:20 +0200

    remove generated app, add default-app in gitignore

--- fghdfjd/decs.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'easygraphql-parser-gamechanger'
--- fghdfjd/src/app/store/models/work.ts
+++
@@ -1,8 +0,0 @@
-import { Employe } from '../models/employe'
-export class Work { 
-          public id!: number;
-  public job?: String;
-  public salary?: String;
-  public empl?: [Employe];
- 
-    }
