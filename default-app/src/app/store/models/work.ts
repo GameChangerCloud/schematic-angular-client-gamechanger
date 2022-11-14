fb940eb149968d97a93fb9ba7489c3f9843b5456
isaoud <imedsaoud@factonics.com>
2022-11-14 14:43:08 +0100

    remove default-app before npm publish

--- default-app/src/app/store/models/work.ts
+++
@@ -1,8 +0,0 @@
-import {Employe} from '../models/employe'
-
-export class Work {
-  public id!: Number;
-  public job?: String;
-  public salary?: string;
-  public empl?: [Employe];
-}
--- default-app/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  cognitoUserPoolId: 'eu-west-1_7IR7kZq1l',
-  cognitoClientId: '2g078qhv9btj5bdcviblrrrjlc',
-  endpoint_uri: 'https://awngefim2i.execute-api.eu-west-1.amazonaws.com/deploy/graphql'
-};
