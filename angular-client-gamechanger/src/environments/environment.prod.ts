f7b245acf88b537e3c4e14f5d6a3079a884560bf
isaoud <imedsaoud@factonics.com>
2022-12-29 17:59:22 +0100

    remove app

--- angular-client-gamechanger/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  api_auth_mechanism: 'none',
-  cognitoUserPoolId: '<cognitoUserPoolId>',
-  cognitoClientId: '<cognitoClientId>',
-  endpoint_uri: '<endpoint_uri>',
-};
--- d/src/app/store/models/studio.ts
+++
@@ -1,6 +0,0 @@
-
-export class Studio { 
-          public name!: String;
-  public turnover?: number;
- 
-    }
